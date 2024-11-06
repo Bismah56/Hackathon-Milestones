bf3999e7c28fae1bb4bce0e564a5d26614087f48
Bismah Abdul Ghaffar <163390239+Bismah56@users.noreply.github.com>
2024-11-06 22:00:39 +0500

    Add files via upload

--- Milestone1/index.ts
+++ Milestone1/index.ts
@@ -6,15 +6,6 @@ const details = document.querySelector('#Details') as HTMLDivElement;
 const show_hidebtn = document.querySelector('#Skills button') as HTMLButtonElement; 
 const skillList = document.querySelector('#Skills span') as HTMLUListElement;
 
-container.addEventListener('mouseenter',()=>{
-    pinfo.style.backgroundColor = '#332f2f';
-    pinfo2.style.backgroundColor = '#332f2f';
-    eduskill.style.backgroundColor = '#f0d8be';
-    details.style.backgroundColor = 'rgb(226, 225, 225)';
-    container.style.border = '4px solid #3d3a3b';
-    container.style.borderRadius = '55px';
-});
-
 show_hidebtn.addEventListener('click',()=>{
     let result = skillList.classList.toggle('hide');   //will return false after removing hide class
     show_hidebtn.innerText = result?'Show skills':'Hide skills';
