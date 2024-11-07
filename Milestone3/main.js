b4c4e48f5af03a9bc40208cc4053097de49e05b4
Bismah Abdul Ghaffar <163390239+Bismah56@users.noreply.github.com>
2024-11-07 15:18:38 +0500

    Add files via upload

--- Milestone3/main.js
+++ Milestone3/main.js
@@ -43,7 +43,7 @@ _userphoto.addEventListener('change', function () {
 form.addEventListener('submit', function (event) {
     event.preventDefault();
     document.body.style.backgroundImage = 'none';
-    document.body.style.backgroundColor = '#F0E4CC';
+    document.body.style.backgroundColor = '#797878';
     form.style.display = 'none';
     resume.style.display = 'inherit';
     name_.textContent = _name.value;
